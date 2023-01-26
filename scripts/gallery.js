e4606d49ba41babf0369029522abe26592d6749e
Naimakhon Nazirova <114399574+Naimakhon@users.noreply.github.com>
2023-01-26 14:45:59 +0500

    Add files via upload

---
+++ scripts/gallery.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const list = document.querySelector('.photo');
+
+list.addEventListener('click', (e) => {
+    if (e.target instanceof HTMLLIElement) {
+
+        document.querySelector('.preview').style.backgroundImage = `url(${e.target.firstElementChild.src}`;
+        document.querySelector('.wrapper').style.display = 'flex';
+    }
+})
+
+document.querySelector('.wrapper').addEventListener('click', () => {
+    document.querySelector('.wrapper').style.display = 'none';
+})
